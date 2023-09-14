e092af4e7e47a6aa1db45b6701bb79d9592e9260
Gam3-git <109097134+Gam3-git@users.noreply.github.com>
2023-09-14 10:20:26 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+const config_set = () => { 
+    return { 
+      "court":"ศาลxxxxxxxxx",
+      "user":"eboardxxxxxxxx",
+      "pass":"Pxxxxxxxx",
+      "ip_webapp":"xx.xx.xx.x:xxxx" 
+    }; 
+}

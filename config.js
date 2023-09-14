7f79aaf0082c117231422f2a9cdca6d18c83587f
Gam3-git <Phannop.n35@gmail.com>
2023-09-14 10:05:58 +0700

    .

---
+++ config.js
@@ -0,0 +1,8 @@
+const config_set = () => { 
+    return { 
+      "court":"ศาลxxxxxxxxx",
+      "user":"eboardxxxxxxxx",
+      "pass":"Pxxxxxxxxxx",
+      "ip_webapp":"xx.xx.xx.x:xxxx" 
+    }; 
+}

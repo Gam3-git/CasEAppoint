48d17cdf6639ea551aaae7b996c7cdf97e42ffff
Gam3-git <109097134+Gam3-git@users.noreply.github.com>
2023-09-14 09:06:01 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const config_set = () => { return { 
   "court":"ศาลจังหวัดสมุทรสงคราม",
-  "user":"eboard",
-  "pass":"P@ssw0rd",
-  "ip_webapp":"10.37.76.2:8089" 
+  "user":"xxxxx",
+  "pass":"xxxxxx",
+  "ip_webapp":"xxxxxxxxxx" 
 }; }
 
 const  get_Token = async () => {

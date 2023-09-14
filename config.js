36f1945d23a047781d3b8c65899450afb410cfa2
Gam3-git <Phannop.n35@gmail.com>
2023-09-14 10:06:48 +0700

    ..

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const config_set = () => { 
     return { 
-      "court":"ศาลxxxxxxxxx",
+      "court":"ศาลxxxxxxxxx00000000000",
       "user":"eboardxxxxxxxx",
       "pass":"Pxxxxxxxxxx",
       "ip_webapp":"xx.xx.xx.x:xxxx" 
